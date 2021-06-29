ce64baa3f5f7f81a519e27d1eebd395500514ea3
Junior Rocco <juniorrocco2020@gmail.com>
2021-06-28 22:26:35 -0300

    FrondEnd Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produto/cadastrar-produto.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+import { HomeComponent } from './components/home/home.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'produtos', component: ListarProdutosComponent},
+  {path:'produtos/cadastrar', component: CadastrarProdutoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
